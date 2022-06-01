eb53738211a2669700483c2293a8186b4354bced
Amey Vivek Borade <77625626+Borade07@users.noreply.github.com>
2022-06-02 00:59:13 +0530

    update 02 June 12:58 am

--- app/src/main/java/com/technic/myapplication/QuizActivity.java
+++ app/src/main/java/com/technic/myapplication/QuizActivity.java
@@ -1,13 +1,27 @@
 package com.technic.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
-
+    Button q1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        q1 = (Button) findViewById(R.id.quiz1);
+
+        q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this,Quiz1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
