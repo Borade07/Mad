84707f02fe8507ba3e2c9f39b5fccffb2814ea88
Amey Vivek Borade <77625626+Borade07@users.noreply.github.com>
2022-06-13 15:46:49 +0530

    First Commit

---
+++ app/src/main/java/com/technic/myapplication/Quiz5Activity.java
@@ -0,0 +1,152 @@
+package com.technic.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Quiz5Activity extends AppCompatActivity {
+    private TextView questionTV,questionNumberTV;
+    private Button option1Btn,option2Btn,option3Btn,option4Btn;
+    private ArrayList<QuizModal> quizModalArrayList;
+    Random random;
+    int currentScore = 0,questionAttemted = 1,currentPos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz5);
+
+        questionTV = findViewById(R.id.idTVQuestion);
+        questionNumberTV = findViewById(R.id.idTVQuestionAttempted);
+        option1Btn = findViewById(R.id.idBtnOption1);
+        option2Btn = findViewById(R.id.idBtnOption2);
+        option3Btn = findViewById(R.id.idBtnOption3);
+        option4Btn = findViewById(R.id.idBtnOption4);
+
+        quizModalArrayList = new ArrayList<>();
+        random = new Random();
+
+        getQuizQuestion(quizModalArrayList);
+        currentPos = random.nextInt(quizModalArrayList.size());
+        setDataToViews(currentPos);
+        option1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option1Btn.getText().toString().trim().toLowerCase()))
+                {
+                    currentScore++;
+                }
+                questionAttemted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+
+            }
+        });
+        option2Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option2Btn.getText().toString().trim().toLowerCase()))
+                {
+                    currentScore++;
+                }
+                questionAttemted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+
+
+
+            }
+        });
+
+        option3Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option3Btn.getText().toString().trim().toLowerCase()))
+                {
+                    currentScore++;
+                }
+                questionAttemted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+
+            }
+        });
+
+        option4Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option4Btn.getText().toString().trim().toLowerCase()))
+                {
+                    currentScore++;
+                }
+                questionAttemted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+
+            }
+        });
+
+
+    }
+    private void showBottomSheet(){
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(Quiz5Activity.this);
+        View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.score_bottom_sheet,(LinearLayout)findViewById(R.id.idLLScore));
+        TextView scoreTV = bottomSheetView.findViewById(R.id.idTVScore);
+        Button restartQuizBtn = bottomSheetView.findViewById(R.id.idBtnRestart);
+        scoreTV.setText("Your Score is \n"+currentScore + "/10");
+        restartQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+                questionAttemted =1;
+                currentScore = 0;
+                bottomSheetDialog.dismiss();
+
+            }
+        });
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+    }
+
+    private void setDataToViews(int currentPos){
+        questionNumberTV.setText("Questions Attempted :"+questionAttemted +"/10");
+        if(questionAttemted == 10){
+            showBottomSheet();
+        }
+        else  {
+
+            questionTV.setText(quizModalArrayList.get(currentPos).getQuestion());
+            option1Btn.setText(quizModalArrayList.get(currentPos).getOption1());
+            option2Btn.setText(quizModalArrayList.get(currentPos).getOption2());
+            option3Btn.setText(quizModalArrayList.get(currentPos).getOption3());
+            option4Btn.setText(quizModalArrayList.get(currentPos).getOption4());
+
+        }
+
+    }
+
+    private void getQuizQuestion(ArrayList<QuizModal> quizModalArrayList ) {
+
+        quizModalArrayList.add(new QuizModal("Question 1 ","Q1 O1","Q1 O2","Q1 O3","Q1 O4","Q1 O3"));
+        quizModalArrayList.add(new QuizModal("Question 2 ","Q2 O2","Q2 O2","Q2 O3","Q2 O4","Q2 O3"));
+        quizModalArrayList.add(new QuizModal("Question 3 ","Q3 O3","Q3 O2","Q3 O3","Q3 O4","Q3 O3"));
+        quizModalArrayList.add(new QuizModal("Question 4 ","Q4 O4","Q4 O2","Q4 O3","Q4 O4","Q4 O3"));
+        quizModalArrayList.add(new QuizModal("Question 5 ","Q5 O4","Q5 O2","Q5 O3","Q5 O4","Q5 O3"));
+        quizModalArrayList.add(new QuizModal("Question 6 ","Q6 O4","Q6 O2","Q6 O3","Q6 O4","Q6 O3"));
+        quizModalArrayList.add(new QuizModal("Question 7 ","Q7 O4","Q7 O2","Q7 O3","Q7 O4","Q7 O3"));
+        quizModalArrayList.add(new QuizModal("Question 8 ","Q8 O4","Q8 O2","Q8 O3","Q8 O4","Q8 O3"));
+        quizModalArrayList.add(new QuizModal("Question 9 ","Q9 O4","Q9 O2","Q9 O3","Q9 O4","Q9 O3"));
+        quizModalArrayList.add(new QuizModal("Question 10 ","Q10 O4","Q10 O2","Q10 O3","Q10 O4","Q10 O3"));
+    }
+}
