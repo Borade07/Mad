afe9ff9826ada78c3f393766adb54ec62fc37eb7
Amey Vivek Borade <77625626+Borade07@users.noreply.github.com>
2022-06-16 23:41:18 +0530

    remove firebase

--- app/src/main/java/com/technic/myapplication/PptActivity.java
+++ app/src/main/java/com/technic/myapplication/PptActivity.java
@@ -8,10 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+
 
 import java.util.ArrayList;
 import java.util.List;
