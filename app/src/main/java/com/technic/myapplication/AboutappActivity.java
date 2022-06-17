93aa33142a69f55666f8efc77b507c5677a60633
Amey Vivek Borade <77625626+Borade07@users.noreply.github.com>
2022-06-17 13:29:36 +0530

    final push of the project

--- app/src/main/java/com/technic/myapplication/AboutappActivity.java
+++ app/src/main/java/com/technic/myapplication/AboutappActivity.java
@@ -3,11 +3,15 @@ package com.technic.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class AboutappActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutapp);
+        PDFView pdfView= findViewById(R.id.aboutUS);
+        pdfView.fromAsset("aboutUS.pdf").load();
     }
 }
